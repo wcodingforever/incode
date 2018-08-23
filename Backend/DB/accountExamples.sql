f890fccb3f88db0e49702fd91887b0d1b8f7f60b
Yena <vovv760@gmail.com>
2018-08-23 16:57:24 +0900

    I added a file for accounts for testing.;

---
+++ Backend/DB/accountExamples.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`username`, `password`, `email`, `birth`, `admin`) 
+VALUES ("user1", "user1pwpw", "user1@email.com", "1989-01-30", "0"),
+("admin1", "admin1pwpw", "admin1@email.com", "1989-01-30", "1");
